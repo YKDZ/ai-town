4667f86382e9447400c03dc49b828754894c7fd7
YKDZ <3070799584@qq.com>
2025-12-17 14:34:35 +0800

    chore(git): remove ignored files from VCS per .gitignore (.idea, .env)

---
+++ src/entities/location.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from typing import List, Optional
+
+class LocationType(Enum):
+    SQUARE = "Square"
+    SALOON = "Saloon"
+    HOME = "Home"
+    LIBRARY = "Library"
+
+
+class Location(BaseModel):
+    name: str
+    english_name: Optional[str] = None
+    type: LocationType
+    description: str
+    connected_locations: List[str] = []
+    coordinates: tuple[int, int] = (0, 0)  # 渲染用
+    notices: List[Notice] = []
